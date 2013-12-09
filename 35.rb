fcacd44484d7e971ce81b5854193416136666855
Mark Nachazel <marknach@vt.edu>
2013-12-09 14:45:38 -0500

    slightly shorter #35

--- 35.rb
+++ 35.rb
@@ -1,1 +1,1 @@
-Prime.each(1000000).find_all{|p| 1.upto(p.to_s.length-1).all?{|i| Prime.prime?(p.to_s.chars.to_a.rotate(i).join.to_i)}}.length
+Prime.each(1000000).count{|p| 1.upto(p.to_s.length-1).all?{|i| Prime.prime?(p.to_s.chars.to_a.rotate(i).join.to_i)}}
