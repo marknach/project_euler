3d717d95145aa4802170855b50af9e8cec7cd40a
Mark Nachazel <marknach@vt.edu>
2014-04-11 08:43:29 -0400

    Update 19.rb

--- 19.rb
+++ 19.rb
@@ -1,3 +1,3 @@
 s = Set.new
-Date.parse('1901-01-01').upto(Date.parse('2000-12-31')).each{|day| s << day.at_beginning_of_month }
+Date.parse('1901-01-01').upto(Date.parse('2000-12-31')).each{|day| s << day.at_beginning_of_month } #wasteful but alternative is much less concise
 s.count{|day| day.sunday?}
